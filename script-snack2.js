51ac303ec923f61a1aba5de380032e7a5ec5c4ab
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-15 16:21:59 +0100

    es. 2 nomi squadre + falli subiti

--- script-snack2.js
+++ script-snack2.js
@@ -8,14 +8,29 @@ const squadreDiCalcio = [
     {"nomeSquadra": "Fiorentina", "puntiFatti": 0, "falliSubiti": 0},
 ]
 
+console.log(squadreDiCalcio)
+
 // creare una funzione che mi generi numeri random 
 // FUNCTION
 function numRandom(min, max){
-    Math.floor(Math.random() * ((max - min) + 1) + min)
+    return Math.floor(Math.random() * ((max - min) + 1) + min) 
 }
 
 // sostituire il numero random al posto di puntiFatti e falliSubiti
-let puntiFatti = numRandom(10, 60)
-let falliSubiti = numRandom(1, 13)
+
+for(let i = 0; i < squadreDiCalcio.length; i++){
+    squadreDiCalcio[i].puntiFatti = numRandom(10, 60);
+    squadreDiCalcio[i].falliSubiti = numRandom(1, 13);
+    console.log("Punti fatti", squadreDiCalcio[i].puntiFatti)
+    console.log("Falli subiti", squadreDiCalcio[i].falliSubiti)
+}
 
 // creare un nuovo array che contiene solamente i nomi delle squadre e i falli subiti
+let nuovoArray = [];
+
+for(let i = 0; i < squadreDiCalcio.length; i++){
+    nuovoArray.push(squadreDiCalcio[i].nomeSquadra);
+    nuovoArray.push(squadreDiCalcio[i].falliSubiti);
+}
+
+console.log(nuovoArray)
